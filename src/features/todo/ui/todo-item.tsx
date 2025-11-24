0b1daf91050533d68796d12fdd12e8505ceeb3ed
redly31 <redlyyyyyyyyyyy@gmail.com>
2025-11-24 22:55:01 +0500

    fix

--- src/features/todo/ui/todo-item.tsx
+++ src/features/todo/ui/todo-item.tsx
@@ -3,21 +3,19 @@ import type { Todo } from "../../../shared/model/Todo"
 
 type TodoItemProps = {
   todo: Todo
-  completeButton: ReactNode
-  deleteButton: ReactNode
+  children: ReactNode
 }
 
 export default function TodoItem(props: TodoItemProps) {
-  const { todo, completeButton, deleteButton } = props
+  const { todo, children } = props
   return (
-    <article className="flex justify-between">
-      <div className="border-neutral-700 p-4 border-2 flex gap-2 items-center w-full">
-        {completeButton}
+    <article className="flex gap-2 justify-between border-neutral-700 border-2 items-center">
+      <div className="p-4 flex items-center w-full">
         <p className={todo.completed ? `line-through text-neutral-400` : ""}>
           {todo.title}
         </p>
       </div>
-      {deleteButton}
+      {children}
     </article>
   )
 }
--- src/features/todo/ui/todo-list.tsx
+++ src/features/todo/ui/todo-list.tsx
@@ -1,6 +1,4 @@
 import TodoItem from "./todo-item"
-import CompleteTodoButton from "./complete-todo-button"
-import DeleteTodoButton from "./delete-todo-button"
 import { useGetTodos } from "../model/use-get-todos"
 import useDeleteTodo from "../model/use-delete-todo"
 import useCompleteTodo from "../model/use-complete-todo"
@@ -14,25 +12,60 @@ export function TodoList() {
     <section className="flex flex-col gap-4 w-2xl mt-8">
       <h2 className="text-2xl font-bold">Todo ({data.length})</h2>
       {data.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          completeButton={
-            <CompleteTodoButton
-              completed={todo.completed}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                completeTodo(todo.id, e)
-              }
-            />
-          }
-          deleteButton={
-            <DeleteTodoButton
-              onClick={() => deleteTodo(todo.id)}
-              isPending={isPending && deletingId === todo.id}
-            />
-          }
-        />
+        <TodoItem key={todo.id} todo={todo}>
+          <CompleteTodoButton
+            completed={todo.completed}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              completeTodo(todo.id, e)
+            }
+          />
+
+          <DeleteTodoButton
+            onClick={() => deleteTodo(todo.id)}
+            isPending={isPending && deletingId === todo.id}
+          />
+        </TodoItem>
       ))}
     </section>
+  )
+}
+
+type CompleteTodoButtonProps = {
+  onChange: React.ChangeEventHandler<HTMLInputElement>
+  completed: boolean
+}
+
+function CompleteTodoButton({ onChange, completed }: CompleteTodoButtonProps) {
+  return (
+    <input
+      className="transition-colors w-8 h-8"
+      type="checkbox"
+      checked={completed}
+      onChange={onChange}
+    />
+  )
+}
+
+type DeleteTodoButtonProps = {
+  onClick: () => void
+  isPending: boolean
+}
+
+export default function DeleteTodoButton({
+  onClick,
+  isPending,
+}: DeleteTodoButtonProps) {
+  return (
+    <button
+      disabled={isPending}
+      onClick={onClick}
+      className={`self-stretch flex-shrink-0 px-8 transition-colors ${
+        isPending
+          ? "bg-neutral-700 hover:bg-neutral-700 !cursor-progress"
+          : "bg-neutral-700 hover:bg-indigo-500"
+      }`}
+    >
+      {isPending ? "Deleting..." : "Delete"}
+    </button>
   )
 }
